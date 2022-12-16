ed18763ab15ee87e751dca701f384f3882445226
Ibrahima Capo-Chichi <40813947+icapochi@users.noreply.github.com>
2022-12-16 04:54:10 -0500

    Made users sign in/up, interface changes, communication of data between pages

--- supabase-app/src/pages/Home.js
+++ supabase-app/src/pages/Home.js
@@ -1,11 +1,34 @@
-import { Box, Text, VStack } from "@chakra-ui/react";
+import { Box, Button, Text, VStack } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { supabase } from "../supabase";
 
 
-export default function Home(){
+
+export default function Home(props){
+    
+    const [user, setUser] = useState({})
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        supabase.auth.getUser().then((value) => {
+            if (value.data?.user){
+                console.log(value.data.user)
+                setUser(value.data.user)
+            }
+        })
+    }, [])   
+
+    const signOut = async function(){
+        
+        const { error } = await supabase.auth.signOut()
+        navigate('/')
+    }
     return(
         <VStack>
             <Box>
-                <Text fontSize='2xl'>Hello Za Warudo</Text>
+                <Text fontSize='3xl'>{user.email}</Text>
+                <Button onClick={signOut}>Sign Out</Button>
             </Box>
         </VStack>
     )
